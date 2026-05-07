b279791928d267bf1a85e2460275937bfcf27bf3
Soroxik <104679127+Soroxik@users.noreply.github.com>
2026-05-07 21:05:28 +0200

    Add files via upload
    Figured out how to conver decimal to binary system and how to count taken and free bytes then calculate available hosts

--- Program.cs
+++ Program.cs
@@ -32,18 +32,32 @@ namespace Aplikacja
             // Koniec tablicy do adresu IP
             // Tablica do maski podsieci
             int[] Maska = new int[4];
+            bool mniejszyOktet = false;
             for (int i = 0; i < 4; i++)
             {
                 Console.WriteLine("Podaj oktet Maski");
                 int oktetM = int.Parse(Console.ReadLine());
-                if (oktetM >= 0 && oktetM <= 255)
+                int[] poprawneOktety = { 255, 254, 252, 248, 240, 224, 192, 128, 0 };
+                if (!poprawneOktety.Contains(oktetM))
                 {
-                    Maska[i] = oktetM;
+                    Console.WriteLine("Nieprawidłowy oktet. Podaj poprawną maskę.");
+                    i--;
+                    // Sprawdza czy podanyy oktet jest poprawny
+                }
+                else if(mniejszyOktet && oktetM !=0)
+                {
+                    Console.WriteLine("Po podaniu oktetu mniejszego niz 255 , kolejne oktety musza byc rowne 0");
+                    i--;
+                    // Sprawdza czy podany oktet jest mniejszy niz 255 , jesli tak to kolejne oktety musza byc rowne 0
                 }
                 else
-                {
-                    Console.WriteLine("Nieprawidłowy oktet. Podaj wartość od 0 do 255.");
-                    i--; // Powtórz iterację dla tego oktetu
+                { 
+                    Maska[i] = oktetM; 
+                    if (oktetM != 255)
+                    {
+                        mniejszyOktet = true;
+                    }
+                    // Jesli podany oktet jest mniejszy niz 255 to ustawia boola na true a nastepnie wymusza aby kolejne oktety byly rowne 0
                 }
             }
             Console.WriteLine("Twoja maska to:");
@@ -56,8 +70,21 @@ namespace Aplikacja
             Console.WriteLine();
             // Koniec tablicy do maski podsieci
             // Konwersja oktetow maski na binarny
-
+            Console.WriteLine("Maska w postaci binarnej:");
+            int mozliweBity = 32;
+            int sumaZajetychBitow = 0;
+            foreach (int oktetM in Maska)
+            {
+                string oktetBin = Convert.ToString(oktetM, 2) /* "2" oznacza system na jaki chce zmienic liczbe  */ .PadLeft(8, '0') /* PadLeft ustawia String od lewej do prawej , 8 oznacza max liczbe znakow a '0' oznacza czym uzupelnic brakujace znaki */;
+                Console.WriteLine(oktetBin);
+                int liczbaZajetychBitow = oktetBin.Count(c => c == '1'); // Count liczy ile jest znakow '1' w stringu
+                sumaZajetychBitow += liczbaZajetychBitow;
+            }
+            int wolneBity = mozliweBity - sumaZajetychBitow;
+            int liczbaDostepnychHostow = (int)Math.Pow(2, wolneBity) - 2;
+            Console.WriteLine($"Liczba zajętych bitów: {sumaZajetychBitow}");
+            Console.WriteLine($"Liczba dostępnych hostów w tej podsieci: {liczbaDostepnychHostow}");
         }
     }
 }
-            
+
